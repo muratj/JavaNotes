a3a93d20f273de6f6354f2c77d029a619a367259
murat <muratj.tm@gmail.com>
2021-02-04 21:48:46 -0500

    reverse string

---
+++ src/com/murat/Reverse.java
@@ -0,0 +1,12 @@
+package com.murat;
+
+public class Reverse {
+    static String reverse(String s) {
+        String r = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            r += s.charAt(i);
+        }
+
+        return r;
+    }
+}
